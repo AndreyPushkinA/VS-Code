0dd44b4957d11d8427fe8f41dcd6e3d583e5a7ad
AndreyPushkinA <57715810+AndreyPushkinA@users.noreply.github.com>
2022-12-06 19:28:25 +0400

    Update indeed_selenium.py

--- Indeed/indeed_selenium.py
+++ Indeed/indeed_selenium.py
@@ -9,44 +9,34 @@ from csv import writer
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
+SEARCH_QUERIES = ["data+engineer", "terraform"]
+start_url = "https://www.indeed.com/jobs?q=data+engineer&sc=0kf%3Ajt%28contract%29%3B&sort=date&fromage=1&vjk=607b75599f92b916"
 
-start_url = "https://www.indeed.com/jobs?q=data+engineer&sort=date&fromage=1"
-driver.get(start_url)
-page_source = driver.page_source
-selector = Selector(text=page_source)
-links = selector.xpath('//a[contains(@class, "jcs-JobTitle")]/@href').extract()
-page_num = 0
-amount = 0
-for link in links:
-    page_num +=1 
-    amount +=1
-    link = "https://indeed.com" + link
-    driver.execute_script("window.open('');")
-    driver.switch_to.window(driver.window_handles[page_num])
+def parse(link):
     driver.get(link)
-    time.sleep(randint(1,5))
-    link_page = Selector(text=driver.page_source)
-    name = link_page.xpath('//h1[contains(@class, "jobsearch-JobInfoHeader")]/text()').extract_first()
-    company_name = link_page.xpath('//div[contains(@class, "jobsearch-InlineCompanyRating-companyHeader")]/a/text()').extract_first()
-    desc = '\n'.join(link_page.xpath('//div[contains(@id, "jobDescriptionText")]//text()').extract())
-    print(name, company_name, desc)
-next_page =  selector.xpath('//a[@aria-label="Next Page"]/@href').extract_first()
-print(next_page)
-while next_page:
-    driver.get('https://indeed.com' + next_page)
-    sel = Selector(text=driver.page_source)
-    links = sel.xpath('//a[contains(@class, "jcs-JobTitle")]/@href').extract()
+    page_source = driver.page_source
+    selector = Selector(text=page_source)
+    links = selector.xpath('//a[contains(@class, "jcs-JobTitle")]/@href').extract()
+    next_page =  selector.xpath('//a[@aria-label="Next Page"]/@href').extract_first()
+    parse_details(links)
+    if next_page:
+        next_page = "https://indeed.com" + next_page
+        parse(next_page)
+
+def parse_details(links):
     page_num = 0
     for link in links:
-        page_num +=1 
         link = "https://indeed.com" + link
+        page_num +=1
         driver.get(link)
-        amount+=1
-        page_desc = Selector(text=driver.page_source)
         time.sleep(randint(1,5))
-        name = page_desc.xpath('//h1[contains(@class, "jobsearch-JobInfoHeader")]/text()').extract_first()
-        company_name = page_desc.xpath('//div[contains(@class, "jobsearch-InlineCompanyRating-companyHeader")]/a/text()').extract_first()
-        desc = '\n'.join(page_desc.xpath('//div[contains(@id, "jobDescriptionText")]//text()').extract())
+        link_page = Selector(text=driver.page_source)
+        name = link_page.xpath('//h1[contains(@class, "jobsearch-JobInfoHeader")]/text()').extract_first()
+        company_name = link_page.xpath('//div[contains(@class, "jobsearch-InlineCompanyRating-companyHeader")]/a/text()').extract_first()
+        desc = '\n'.join(link_page.xpath('//div[contains(@id, "jobDescriptionText")]//text()').extract())
         print(name, company_name, desc)
-    next_page = sel.xpath('//a[@aria-label="Next Page"]/@href').extract_first()
-print(amount)
+
+def main():
+    parse(start_url)
+
+main()
